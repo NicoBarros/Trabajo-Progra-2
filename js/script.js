8ed1557af76363ff032191ec182362d53f13fda7
Nico:3 <167337131+NicoBarros@users.noreply.github.com>
2024-06-06 10:46:27 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -141,11 +141,17 @@ function validarnumero() {
     if (phone === '') {
         phoneError.textContent = 'El número de teléfono es obligatorio.';
         return false;
-    } else if (isNaN(phone)) {
-        phoneError.textContent = 'El número de teléfono debe ser numérico.';
+    } else if (isNaN(telefono.slice(1))) {
+        phoneError.textContent = "Por favor ingrese un numero de telefono válido";
+        return false;
+    } else if (!telefono.startsWith("+")) {
+       phoneError.textContent = "El número de telefonico debe tener +";
+        return false;
+    } else if (telefono.length > 16 || telefono.length < 12) { 
+        phoneError.textContent = "El teléfono debe tener entre 12 y 15 dígitos, incluyendo el '+'";
         return false;
     } else {
-        phoneError.textContent = '';
+        phoneError.textContent = "";
         return true;
     }
 }
